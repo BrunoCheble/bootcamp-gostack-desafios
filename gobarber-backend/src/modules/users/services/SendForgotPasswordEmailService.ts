e3eb5c6408ef05658e570461c2d3405627eba456
brunocheble <contato.cheble@gmail.com>
2020-05-30 00:04:08 +0100

    Aula: Template de emails Instalação do package handlebars; Criação do provider MailTemplate; Implementação do provider dentro do Provider SendMail

--- gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,4 @@
-// import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
-// import User from '../infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
@@ -30,10 +28,20 @@ class SendForgotPasswordEmailService {
     }
 
     const { token } = await this.userTokensRepository.generate(user.id);
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido. ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
