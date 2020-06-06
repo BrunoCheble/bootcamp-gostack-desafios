31628f220335256e7c11f199f05b37a8ab3addc2
brunocheble <contato.cheble@gmail.com>
2020-06-06 13:58:47 +0100

    Aula: Regras do agendamento Desenvolvimento dos testes para a criação de appointments: - should not be able to create an appointment with the same user as provider; - should not be able to create an appointment on a past date; - should not be able to create an appointment before 8am and after 5pm

--- gobarber-backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ gobarber-backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -26,6 +26,19 @@ class CreateAppointmentService {
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError('You can create an appointment between 8am and 5pm');
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError(
+        "You can't create an appointment with the same user as provider",
+      );
+    }
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
