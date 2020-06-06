4fb644189eba15e2c840fc94cd81817a820937db
brunocheble <contato.cheble@gmail.com>
2020-06-07 00:39:34 +0100

    Class: Refatorando testes Install @types/mongodb; Updated FakeNotificationRepository;

--- gobarber-backend/src/modules/notifications/repositories/fakes/FakeNotificationsRepository.ts
+++ gobarber-backend/src/modules/notifications/repositories/fakes/FakeNotificationsRepository.ts
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4';
+import { ObjectID } from 'mongodb';
 
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
@@ -13,7 +13,7 @@ class NotificationsRepository implements INotificationsRepository {
   }: ICreateNotificationDTO): Promise<Notification> {
     const notification = new Notification();
 
-    Object.assign(notification, { id: uuid(), content, recipient_id });
+    Object.assign(notification, { id: new ObjectID(), content, recipient_id });
 
     this.notifications.push(notification);
 
