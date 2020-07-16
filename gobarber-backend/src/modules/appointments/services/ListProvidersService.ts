0d06783fa04c9c1788eb2cc5ed3fecdcb3f09afd
brunocheble <contato.cheble@gmail.com>
2020-07-16 18:42:36 +0100

    Implementação do Cache na lista de providers;

--- gobarber-backend/src/modules/appointments/services/ListProvidersService.ts
+++ gobarber-backend/src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,8 @@ import User from '@modules/users/infra/typeorm/entities/User';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 interface IRequest {
   user_id: string;
 }
@@ -13,13 +15,24 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+
+    let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
+
+    if(!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      this.cacheProvider.save(`providers-list:${user_id}`, users);
+    }
+
     return users;
+
   }
 }
 
