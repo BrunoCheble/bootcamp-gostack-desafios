a47a129d94f7285a8eddd68dc6707fe1166fa87a
brunocheble <contato.cheble@gmail.com>
2020-08-10 13:23:55 +0100

    Aula: Próximo agendamento Estilização do componente "Próximo agendamento" do Dashboard

--- 06-gobarber-web/src/pages/Dashboard/index.tsx
+++ 06-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, Calendar, NextAppointment } from './styles';
 import { useAuth } from '../../hooks/auth';
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
 
@@ -12,9 +12,9 @@ const Dashboard: React.FC = () => {
     <Container>
       <Header>
         <HeaderContent>
-          <img src={logoImg} alt="GoBarber"/>
+          <img src={logoImg} alt="GoBarber" />
           <Profile>
-            <img src={user.avatar_url} alt={user.name}/>
+            <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem vindo,</span>
               <strong>{user.name}</strong>
@@ -23,6 +23,28 @@ const Dashboard: React.FC = () => {
           <button onClick={signOut} type="button"><FiPower /></button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 18</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="https://avatars1.githubusercontent.com/u/5677802?s=460&u=45628f8eaef21432d343000d9665472aad2e93bf&v=4" alt="Bruno Cheble"/>
+              <strong>Bruno Cheble</strong>
+              <span>
+                <FiClock />
+                13:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- 06-gobarber-web/src/pages/Dashboard/styles.tsx
+++ 06-gobarber-web/src/pages/Dashboard/styles.tsx
@@ -6,6 +6,7 @@ export const Header = styled.header`
 padding: 32px 0;
 background: #28262e;
 `;
+
 export const HeaderContent = styled.div`
 display: flex;
 max-width: 1120px;
@@ -28,6 +29,7 @@ button {
   }
 }
 `;
+
 export const Profile = styled.div`
 display: flex;
 align-items: center;
@@ -52,5 +54,89 @@ div {
     color: #ff9000;
   }
 }
+`;
+
+export const Content = styled.main`
+max-width: 1120px;
+margin: 64px auto;
+display: flex;
+`;
+
+export const Schedule = styled.div`
+flex: 1;
+margin-right: 120px;
+font-weight: 500;
+
+h1 {
+  font-size: 36px;
+}
+p {
+  margin-top: 8px;
+  color: #ff9000;
+
+  span + span {
+    margin-left: 8px;
+    padding-left: 8px;
+    border-left: 1px solid #ff9000;
+  }
+}
+`;
+
+export const NextAppointment = styled.div`
+margin-top: 64px;
+
+ > strong {
+  color: #999591;
+  font-size: 20px;
+  font-weight: 400;
+}
+
+div {
+  background: #3e3b47;
+  display: flex;
+  align-items: center;
+  padding: 16px 24px;
+  border-radius: 10px;
+  margin-top: 24px;
+  position: relative;
+
+  &::before {
+    position: absolute;
+    height: 80%;
+    width: 1px;
+    left: 0;
+    top: 10%;
+    content: '';
+    background: #ff9000;
+  }
+
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  strong {
+    color: #fff;
+    margin-left: 24px;
+  }
+
+  span {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    color: #999591;
+
+    svg {
+      color: #ff9000;
+      margin-right: 8px;
+    }
+  }
+}
+`;
+
+
+export const Calendar = styled.div`
+width: 380px;
 `;
 
