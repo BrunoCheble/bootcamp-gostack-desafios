eec347eeee4d15b1c80df42952f78f210bcf0a6c
brunocheble <contato.cheble@gmail.com>
2020-08-10 12:54:25 +0100

    Aula: Criando Header; Para o retorno da autenticação, foi criada uma interface para representar os dados do usuário; Desenvolvimento do header da aplicação;

--- 06-gobarber-web/src/pages/Dashboard/index.tsx
+++ 06-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Header, HeaderContent, Profile } from './styles';
+import { useAuth } from '../../hooks/auth';
+import logoImg from '../../assets/logo.svg';
+import { FiPower } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
-  return <Container>Dashboard</Container>;
+
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber"/>
+          <Profile>
+            <img src={user.avatar_url} alt={user.name}/>
+            <div>
+              <span>Bem vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button onClick={signOut} type="button"><FiPower /></button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
